96da10645d37dfd06db68044f6702fbe2b81675d
Jose Carrillo <82d.jumpmaster@gmail.com>
2015-01-04 04:04:56 -0500

    *

--- js/config.js
+++ js/config.js
@@ -4,3 +4,11 @@ $(function() {
 $(window).on("orientationchange", function() {
 	pikabu.closeSidebars();
 });
+
+$(window).scroll(function(){
+  var sticky = $('.sticky'),
+      scroll = $(window).scrollTop();
+
+  if (scroll >= 50) sticky.prependTo('body').addClass('fixed');
+  else sticky.prependTo('.m-pikabu-container').removeClass('fixed');
+});
