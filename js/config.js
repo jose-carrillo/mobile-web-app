550a3344f8e4dc76603170469e1f6be77c35aa77
Jose Carrillo <82d.jumpmaster@gmail.com>
2015-01-05 10:49:56 -0500

    *

--- js/config.js
+++ js/config.js
@@ -12,14 +12,14 @@ $(document).on('touchmove',function(e){
 });
 //uses body because jquery on events are called off of the element they are
 //added to, so bubbling would not work if we used document instead.
-$('body').on('touchstart','.scrollable',function(e) {
+$('body').on('touchstart','.scroll',function(e) {
   if (e.currentTarget.scrollTop === 0) {
     e.currentTarget.scrollTop = 1;
   } else if (e.currentTarget.scrollHeight === e.currentTarget.scrollTop + e.currentTarget.offsetHeight) {
     e.currentTarget.scrollTop -= 1;
   }
 });
-//prevents preventDefault from being called on document if it sees a scrollable div
-$('body').on('touchmove','.scrollable',function(e) {
+//prevents preventDefault from being called on document if it sees a scroll div
+$('body').on('touchmove','.scroll',function(e) {
   e.stopPropagation();
 });
