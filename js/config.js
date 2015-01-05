1e24e654ffbe22a0d1c2f5375b1b9cd373670308
Jose Carrillo <82d.jumpmaster@gmail.com>
2015-01-05 10:09:39 -0500

    *

--- js/config.js
+++ js/config.js
@@ -1,8 +1,13 @@
 $(function() {
 	pikabu = new Pikabu();
 	FastClick.attach(document.body);
+	checkDeviceStandAlone();
 });
 
 $(window).on("orientationchange", function() {
 	pikabu.closeSidebars();
 });
+
+function checkDeviceStandAlone() {
+	if(window.navigator.standalone == true) {}
+}
