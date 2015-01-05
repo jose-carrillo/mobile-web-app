e982e6e66609253ebbd4a048b15fc2433d5b9da1
Jose Carrillo <82d.jumpmaster@gmail.com>
2015-01-05 10:44:55 -0500

    *

--- js/config.js
+++ js/config.js
@@ -6,29 +6,3 @@ $(function() {
 $(window).on("orientationchange", function() {
 	pikabu.closeSidebars();
 });
-
-function checkDeviceStandAlone() {
-	if (window.navigator.standalone == true) {
-		var overflow = function(el) {
-				el.addEventListener('touchstart', function() {
-					var top = el.scrollTop,
-						totalScroll = el.scrollHeight,
-						currentScroll = top + el.offsetHeight
-					if (top === 0) {
-						el.scrollTop = 1
-					} else if (currentScroll === totalScroll) {
-						el.scrollTop = top - 1
-					}
-				})
-				el.addEventListener('touchmove', function(evt) {
-					if (el.offsetHeight < el.scrollHeight) evt._isScroller = true
-				})
-			}
-		overflow(document.querySelector('#scroll'));
-		document.body.addEventListener('touchmove', function(evt) {
-			if (!evt._isScroller) {
-				evt.preventDefault()
-			}
-		});
-	}
-}
